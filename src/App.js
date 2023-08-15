aae96ca85e54b0974d9c8ca7d6c4bd53d83a4cd2
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-08-15 14:44:12 +0530

    first repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './Components/Calculator';
+import Style from './Components/Style.css';
+
+
+function App() {
+  return (
+   <>
+<Calculator/>
+
+   </>
+  );
+}
+
+export default App;
